305e4f1c5322f5c63df43ccce3ac1f56e9dcea33
Daniel Nita <daniel.nita@hotmail.com>
2024-07-22 19:08:06 +0200

    modulo shared configurado

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,21 +1,15 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { PacientesRegistradosComponent } from './pacientes-registrados/pacientes-registrados.component';
-import { HistorialAltasComponent } from './historial-altas/historial-altas.component';
+import { NgModule } from '@angular/core'; // Importa el decorador NgModule de Angular
+import { CommonModule } from '@angular/common'; // Importa CommonModule que incluye las directivas comunes de Angular como ngIf y ngFor
+import { FormsModule } from '@angular/forms'; // Importa FormsModule para trabajar con formularios en Angular
+import { SharedRoutingModule } from './shared-routing.module'; // Importa el módulo de enrutamiento
 
 @NgModule({
   imports: [
-    CommonModule, 
-    FormsModule
+    CommonModule, // Importa CommonModule para que las directivas comunes estén disponibles en los componentes de este módulo
+    FormsModule, // Importa FormsModule para permitir el uso de formularios en los componentes de este módulo
+    SharedRoutingModule // Importa el módulo de enrutamiento
   ],
-  declarations: [
-    PacientesRegistradosComponent,
-    HistorialAltasComponent
-  ],
-  exports: [
-    PacientesRegistradosComponent,
-    HistorialAltasComponent
-  ]
+  declarations: [],
+  exports: []
 })
-export class SharedModule { }
+export class SharedModule { } // Define y exporta la clase SharedModule, que agrupa componentes, directivas y pipes comunes que pueden ser reutilizados en toda la aplicación
